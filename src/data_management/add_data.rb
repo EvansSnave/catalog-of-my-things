9cca19e3cb18d0a4ebc7e4c756bb54c6e77a611e
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-11-24 14:22:26 -0500

    create data management for genres and albums

--- src/data_management/add_data.rb
+++ src/data_management/add_data.rb
@@ -1,7 +1,11 @@
 require_relative 'data/books_data'
 require_relative 'data/labels_data'
+require_relative 'data/albums_data'
+require_relative 'data/genres_data'
 
 module AddData
   include BooksData
   include LabelsData
+  include AlbumData
+  include GenreData
 end
---
+++ src/data_management/data/albums_data.rb
@@ -0,0 +1,30 @@
+require_relative '../path_finder'
+
+module AlbumData
+  include PathFinder
+
+  def read_all_albums()
+    data = read_file(create_path('albums.json'))
+    data.map { |album| json_to_album(album, genres.find { |item| album['genre']['id'] == item.id }) }
+  end
+
+  def save_album(albums)
+    data = albums.map { |album| album_to_json(album) }
+    write_file(create_path('albums.json'), data)
+  end
+
+  def album_to_json(album)
+    {
+      id: album.id,
+      publish_date: album.publish_date,
+      on_spotify: album.on_spotify,
+      genre: { id: album.genre.id, name: album.genre.name }
+    }
+  end
+
+  def json_to_album(album_json, genre)
+    album = MusicAlbum.new(album_json['id'], album_json['publish_date'], album_json['on_spotify'])
+    album.add_genre(genre)
+    album
+  end
+end
