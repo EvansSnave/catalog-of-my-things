f413103798062f2039576c6179c242bbb3a103bd
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-11-24 15:00:19 -0500

    add games and authors files

---
+++ src/data_management/data/authors_data.rb
@@ -0,0 +1,19 @@
+require_relative '../path_finder'
+
+module AuthorsData
+  include PathFinder
+
+  def read_all_authors
+    
+  end
+
+  def save_authors
+
+  end
+
+  def author_to_json
+  end
+
+  def json_to_author
+  end
+end
